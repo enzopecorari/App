bcd4bcbd0032f435b792838729de1dc59c1244b5
Enzo Pecorari <enzo_pecorari@hotmail.com>
2012-03-13 14:35:39 -0300

    dia 1

--- Dia_1/index.php
+++ Dia_1/index.php
@@ -19,37 +19,7 @@
 
     <div id="result"></div>
   
-    <script>
-    window.onload = function(){ alert("Page loaded"); }
-    $('#alias').focus();
-    
-    
-    $("#alias").keypress(function(event) {
-    if (event.which == 13) {
-        $("#btn").click();
-    }
-    });
-    $("#button").click(function(event) {
-        event.preventDefault();
-        var param = $("#alias").val();
-
-        $.post('api/dispatcher.php',
-            {service: 'welcome.hello', params: {"name": ""+param+""}},
-            function(res) {
-                $("#result").html(res);
-                $("#result").addClass('resultado');
-                $("#result").removeClass('error');      
-            });
-        $("#alias").val('');
-        $("#alias").focus();
-    });
-    $("#result").ajaxError(function() {
-    $("#result").addClass('error');
-    $("#result").removeClass('resultado');
-    $("#result").text("ERROR");  
-    });
-    
-    </script> 
+    <script src="script.js"></script>    
     
     </body>
 </html>
---
+++ Dia_1/script.js
@@ -0,0 +1,33 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+window.onload = function(){ alert("Page loaded"); }
+    $('#alias').focus();
+    
+    
+    $("#alias").keypress(function(event) {
+    if (event.which == 13) {
+        $("#btn").click();
+    }
+    });
+    $("#button").click(function(event) {
+        event.preventDefault();
+        var param = $("#alias").val();
+
+        $.post('api/dispatcher.php',
+            {service: 'welcome.hello', params: {"name": ""+param+""}},
+            function(res) {
+                $("#result").html(res);
+                $("#result").addClass('resultado');
+                $("#result").removeClass('error');      
+            });
+        $("#alias").val('');
+        $("#alias").focus();
+    });
+    $("#result").ajaxError(function() {
+    $("#result").addClass('error');
+    $("#result").removeClass('resultado');
+    $("#result").text("ERROR");  
+    });
