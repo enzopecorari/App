a72151d8a9e9fe23d8e72ed1e5b2355685677cbf
Enzo Pecorari <enzo_pecorari@hotmail.com>
2012-03-14 15:25:21 -0300

    dia 1

---
+++ Dia_2_y_3/pages/sample1.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Sample page 1</title>
+    </head>
+    <body>
+        <header>
+            <h1>This is Sample page 1</h1>
+        </header>
+
+        <div>
+            OK, if you're seeing this inside a jQuery Mobile page...<br />
+            <strong>You did it!</strong><br />
+            :D
+        </div>
+    </body>
+</html>
