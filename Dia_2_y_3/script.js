be65fef304e9bc6276fc1b8c3d6fe3da5e46b9c2
Enzo Pecorari <enzo_pecorari@hotmail.com>
2012-03-14 13:29:16 -0300

    dia 1

--- Dia_2_y_3/script.js
+++ Dia_2_y_3/script.js
@@ -13,22 +13,29 @@
         $("#btn").click();
     }
     });
+    
     $("#btn").click(function(event) {
         var param = $("#alias").val();
-
+        $.mobile.showPageLoadingMsg("d", "Loading", false);
         $.post('api/dispatcher.php',
             {service: 'welcome.hello', params: {"name": ""+param+""}},
             function(res) {
                 var resul = $("#result");
-                resul.html(res);
                 resul.addClass('resultado');
-                resul.removeClass('error');  
+                resul.removeClass('error'); 
+                resul.html(res);
             });
         $("#alias").val('');
         $("#alias").focus();
     });
     $("#result").ajaxError(function(event, request, settings){
-        $(this).html("Error en " + settings.url + ": "+ request.responseText);
         $(this).addClass('error');
         $(this).removeClass('resultado'); 
+        $(this).html("Error en " + settings.url + ": "+ request.responseText);
     }); 
+    
+    $(document).keypress(function(event) {
+        if (event.which != 13) { 
+        $.mobile.hidePageLoadingMsg();
+    }
+    });
