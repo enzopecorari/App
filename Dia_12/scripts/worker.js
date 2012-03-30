bb25d091e3f499c84ab206a40b182074050d63f0
Enzo Pecorari <enzo_pecorari@hotmail.com>
2012-03-29 23:06:48 -0300

    Dia 12 listo para code review

---
+++ Dia_12/scripts/worker.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+setInterval("showmesagge()", 60000);    
+
+
+
+function showmesagge(){ 
+    alert('minute');
+} 
